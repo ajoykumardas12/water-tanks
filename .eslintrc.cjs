59c92fd2ab1b07267553d432880ec49d0ccb1524
Ajoy Kumar Das <87164140+ajoykumardas12@users.noreply.github.com>
2024-01-28 21:33:32 +0530

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: [
+    "dist",
+    "node_modules/*",
+    ".eslintrc.cjs",
+    ".prettierrc.js",
+    "tailwind.config.js",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+      },
+    ],
+  },
+};
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,5 @@
+const HelloWorld = () => {
+  return <div className="bg-green-300 lg:bg-green-500">Hello World test</div>;
+};
+
+export default HelloWorld;
